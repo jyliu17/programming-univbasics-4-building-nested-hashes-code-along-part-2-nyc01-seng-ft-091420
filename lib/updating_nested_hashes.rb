ccc5b81bd06a62157ea8086475989d00ac9ddc53
Johnson Liu <jyliu17@gmail.com>
2020-08-27 00:30:18 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -66,7 +66,7 @@ prince = { name: "Prince Escalus" }
 apothecary = {name: "Apothecary" }
 
 epic_tragedy[:additional_characters] << prince
-
+epic_tragedy[:additional_characters] << apothecary 
 
    # After your code, the following line will return the altered hash
    epic_tragedy
